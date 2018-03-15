1a0083b75571c18e1e386a245a73b2246814b99c
AvihaiBinderPC\AvihaiBinder <avihaibinder@gmail.com>
2018-03-15 18:54:37 +0200

    updated the options activity

--- app/src/main/java/com/txstyle/Options.java
+++ app/src/main/java/com/txstyle/Options.java
@@ -1,7 +1,9 @@
 package com.txstyle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Options extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class Options extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+    }
+
+    public void onColorPickerClick(View view) {
+    }
+
+    public void onGalleryClick(View view) {
+    }
+
+    public void onCameraClick(View view) {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
