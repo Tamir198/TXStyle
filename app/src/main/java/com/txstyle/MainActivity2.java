c19b4d0260b38aae3b20bcfe7c4be89d2640bb70
Tamir Abutbul <1tamir198@gmail.com>
2018-03-16 03:42:37 +0200

    mainactivity2

---
+++ app/src/main/java/com/txstyle/MainActivity2.java
@@ -0,0 +1,195 @@
+package com.txstyle;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
+
+/**
+ * Created by admin on 16/03/2018.
+ */
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
+    RelativeLayout myBackground;
+    ImageView colorPicker;
+    ImageView textEfects;
+
+    private int xDelta;
+    private int yDelta;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        colorPicker = findViewById(R.id.main_color_picker);
+        myBackground = findViewById(R.id.main_background);
+
+        textEfects = findViewById(R.id.main_text_effect);
+
+        textView = findViewById(R.id.textView);
+//        Intent i = new Intent(this,splahScreenActivity.class);
+//        startActivity(i);
+
+        textView.setOnTouchListener(onTouchListener());
+
+
+        textEfects.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = LayoutInflater.from(MainActivity2.this);
+
+                View textEfectView = inflater.inflate(R.layout.dialog_text_effect, null);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity2.this);
+
+                builder.setView(textEfectView);
+
+//                final EditText contactName = (EditText) textEfectView.findViewById(R.id.contact_name);
+//                final EditText contactPhone = (EditText) textEfectView.findViewById(R.id.contact_phone);
+
+
+                builder
+                        .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                builder.create();
+
+                builder.show();
+
+            }
+        });
+
+
+
+        colorPicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int color = Color.TRANSPARENT;
+                Drawable background = myBackground.getBackground();
+                if (background instanceof ColorDrawable)
+                    color = ((ColorDrawable) background).getColor();
+
+                ColorPickerDialogBuilder
+                        .with(MainActivity2.this)
+                        .setTitle("Choose color")
+                        .initialColor(color)
+                        .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                        .density(12)
+                        .setOnColorSelectedListener(new OnColorSelectedListener() {
+                            @Override
+                            public void onColorSelected(int selectedColor) {
+//                        toast("onColorSelected: 0x" + Integer.toHexString(selectedColor));
+                            }
+                        })
+                        .setPositiveButton("ok", new ColorPickerClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                                changeBackgroundColor(selectedColor);
+                            }
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .build()
+                        .show();
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        textView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                System.out.println(textView.getHeight()); //height is ready
+                textView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+    }
+
+    public void changeBackgroundColor(int selectedColor){
+        myBackground.setBackgroundColor(selectedColor);
+
+    }
+
+
+    private View.OnTouchListener onTouchListener() {
+        return new View.OnTouchListener() {
+
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+
+                final int x = (int) event.getRawX();
+                final int y = (int) event.getRawY();
+
+                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams)
+                                view.getLayoutParams();
+
+                        xDelta = x - lParams.leftMargin;
+                        yDelta = y - lParams.topMargin;
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
+                                .getLayoutParams();
+                        layoutParams.leftMargin = x - xDelta;
+                        layoutParams.topMargin = y - yDelta;
+                        layoutParams.rightMargin = 0;
+                        layoutParams.bottomMargin = 0;
+                        view.setLayoutParams(layoutParams);
+                        break;
+                }
+                myBackground.invalidate();
+                return true;
+            }
+        };
+    }
+
+
+}
